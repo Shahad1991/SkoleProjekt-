e067d23edbe52cf6425d1999fee19dfbc48febc8
Shahad Al_ jawashee <shahad1nazar@gmail.com>
2024-11-04 21:40:14 +0100

    hello, jeg har lavet den ny komponent

---
+++ NewComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="new-component">
+        <h1>Hello from NewComponent!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NewComponent'
+}
+</script>
+
+<style scoped>
+.new-component {
+    text-align: center;
+}
+</style>
