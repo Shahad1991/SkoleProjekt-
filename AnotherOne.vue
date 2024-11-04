1efac959be6bf5d1ac6fdd856f9257ddd11de282
Shahad Al_ jawashee <shahad1nazar@gmail.com>
2024-11-04 22:08:11 +0100

    hello her er test

---
+++ AnotherOne.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="another-one">
+        <h1>Hello from AnotherOne component!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AnotherOne'
+}
+</script>
+
+<style scoped>
+.another-one {
+    text-align: center;
+    margin-top: 20px;
+}
+</style>
